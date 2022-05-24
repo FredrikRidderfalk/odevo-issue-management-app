84cd1f4c4015ffbdfea6a61f5dc946dc2f03510b
Fredrik Ridderfalk <ridderfalk@gmail.com>
2022-05-24 17:22:56 -0400

    add grid

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WelcomeMessageComponent } from './Components/WelcomeMessageComponent/welcome-message.component';
 import { ReportIssueComponent } from './Components/ReportIssueComponent/report-issue.component';
+import { NavigationComponent } from './Components/NavigationComponent/navigation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WelcomeMessageComponent,
-    ReportIssueComponent
+    ReportIssueComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
