544a77958ea90bf1ee641cc7453ea4da2904ea2e
Fredrik Ridderfalk <ridderfalk@gmail.com>
2022-05-26 14:07:00 -0400

    add info as variables

--- src/app/Components/new-tasks/new-tasks.component.ts
+++ src/app/Components/new-tasks/new-tasks.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-tasks.component.sass']
 })
 export class NewTasksComponent implements OnInit {
+  name = "Brf Olskroken"
+  info = "The laundry room for stairwell 2B has been experiencing a leak since yesterday coming fr..."
+  contact = "Alex Skarsgård"
+  date = "2022-07-11"
+  tagSource = "/assets/water-tag2.png"
+  calendarIcon = "calendar_today"
+
+  calendarIconElement = document.querySelector("#calendar-icon")
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  // updateCalendarIcon () {
+  // }
 
 }
