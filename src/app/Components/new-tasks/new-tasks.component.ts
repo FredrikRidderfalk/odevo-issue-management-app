c1324ac76d4911ce3aee7bbed7837ed6d9fb9962
Fredrik Ridderfalk <ridderfalk@gmail.com>
2022-05-27 12:46:42 -0400

    add reported issues to new task component

--- src/app/Components/new-tasks/new-tasks.component.ts
+++ src/app/Components/new-tasks/new-tasks.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { task } from 'src/app/models/task';
 
 @Component({
   selector: 'app-new-tasks',
@@ -13,14 +14,67 @@ export class NewTasksComponent implements OnInit {
   tagSource = "/assets/water-tag2.png"
   calendarIcon = "calendar_today"
 
-  calendarIconElement = document.querySelector("#calendar-icon")
+  tasks!: task[]
+
+  inputBrf: string = ""
+  inputDescription: string = ""
+  inputContact: string = ""
 
   constructor() { }
 
   ngOnInit(): void {
+    this.tasks = [
+      {
+        brf: "Brf Olskroken",
+        description: "Something about a leak.",
+        contact: "Alex Skarsgård",
+        read: false,
+        addedToCalendar: false,
+      },
+      {
+        brf: "Brf Guldheden",
+        description: "A light has gone out.",
+        contact: "Anna Annedal",
+        read: true,
+        addedToCalendar: true,
+      },
+    ]
   }
 
-  // updateCalendarIcon () {
-  // }
+  markTaskAsRead(id: number) {
+    this.tasks.map((value, i) => {
+      if(i === id) value.read = true
+
+      return value
+    })
+  }
+
+  addToCalendar(id: number) {
+    this.tasks.map((value, i) => {
+      if(i === id) value.addedToCalendar = true
+
+      return value
+    })
+  }
+
+  markTaskAsResolved(id: number) {
+    this.tasks = this.tasks.filter((value, i) => i !== id)
+  }
+
+  reportIssue() {
+    // Add issue to tasks array
+    this.tasks.push({
+      brf: this.inputBrf,
+      description: this.inputDescription,
+      contact: this.inputContact,
+      read: false,
+      addedToCalendar: false
+    })
+
+    // Clear input fields on submit
+    this.inputBrf = ""
+    this.inputDescription = ""
+    this.inputContact = ""
+  }
 
 }
