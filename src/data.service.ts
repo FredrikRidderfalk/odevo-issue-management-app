9721eb997a8da6df682d35945cc3770528c7c822
Fredrik Ridderfalk <ridderfalk@gmail.com>
2022-05-28 08:35:32 -0400

    add data service

--- src/data.service.ts
+++ src/data.service.ts
@@ -1,14 +1,41 @@
 import { Injectable } from "@angular/core";
-import { BehaviorSubject } from "rxjs";
+import { BehaviorSubject, Observable, Subject } from "rxjs";
 
-@Injectable()
-export class DataService {
-    private messageSource = new BehaviorSubject<string>("default message")
-    currentMessage = this.messageSource.asObservable()
+// @Injectable()
+// export class DataService {
 
-    constructor() { }
+    // attempt 1 - failed
+//     // private messageSource = new BehaviorSubject<any>("default message")
+//     // currentMessage = this.messageSource.asObservable()
+//     // changeMessage(tasks: any) {
+//     //     this.messageSource.next(tasks)
+//     // }
 
-    changeMessage(tasks: any) {
-        this.messageSource.next(tasks)
-    }
-}
+//     constructor() { }
+
+    // attempt 2 - failed
+//     public editDataDetails: any = [];
+//     public subject = new Subject<any>();
+//     private messageSource = new BehaviorSubject(this.editDataDetails);
+//     currentMessage = this.messageSource.asObservable();
+//     changeMessage(message: any) {
+//     this.messageSource.next(message)
+//     }
+// }
+
+// @Injectable()
+// export class DataSharingService {
+//     private subject = new Subject<any>()
+
+//     sendData(data: any) {
+//         this.subject.next({ text: data })
+//     }
+
+//     clearData() {
+//         this.subject.next()
+//     }
+
+//     getData(): Observable<any> {
+//         return this.subject.asObservable()
+//     }
+// }
